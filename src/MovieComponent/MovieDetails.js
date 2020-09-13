f3cf0eb3fd5849ce7b97f41a29a6cb6c6f58860c
Sandeep Kumar Saxena <sandeep.a.saxena@capgemini.com>
2020-09-13 23:40:39 +0530

    Added routing

--- src/MovieComponent/MovieDetails.js
+++ src/MovieComponent/MovieDetails.js
@@ -1,21 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import AvatarGroup from '@material-ui/lab/AvatarGroup';
 import Tooltip from '@material-ui/core/Tooltip';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
+import * as MoviesApi from "../crud/MoviesCrud";
 
-export default function MovieDetails(props) {
-    const imdbID = props.location.state.imdbID;
-    console.log(imdbID);
-  return (
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      overflow: 'hidden',
+      backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+      width: 500,
+      height: 450,
+    },
+    icon: {
+      color: 'rgba(255, 255, 255, 0.54)',
+    },
+  }));
+
+
   
-    <AvatarGroup max={3}>
-      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-      <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-      <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-      <Tooltip title="Foo • Bar • Baz">
-        <Avatar>+3</Avatar>
-      </Tooltip>
-      <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-    </AvatarGroup>
+export default function MovieDetails(props) {
+    const classes = useStyles();
+    const imdbID = props.location.state.imdbID;
+
+
+    const [state, setState] = useState({
+       MovieDetails:null
+    });
+    useEffect(() => {
+        console.log(imdbID)
+        MoviesApi.getMoviesDetail(imdbID).then((data) => {
+     
+          
+            setState({ ...state, MovieDetails:data.data });
+           }).catch((error) => {
+               console.log("Error fetching users", error);
+           });
+
+    }, []);
+
+    if (!state.MovieDetails) {
+        return null;
+    }
+  return (
+
+  <div>
+    
+    {console.log(state.MovieDetails[0].stills)}
+<GridList cellHeight={160} className={classes.gridList} cols={3}>
+    {state.MovieDetails[0].stills.map((tile) => (
+      <GridListTile key={tile}>
+        <img src={tile} alt={tile} />
+      </GridListTile>
+    ))}
+  </GridList>
+
+    
+  </div>
   );
+
 }
--- src/crud/MoviesCrud.js
+++ src/crud/MoviesCrud.js
@@ -6,3 +6,9 @@ export const Movies_CRUD_URL = constants.API_BASE_URL + "Movies";
 export function getMoviesList(organizationId) {
     return axios.get(Movies_CRUD_URL+"/MovieList");
 }
+
+export function getMoviesDetail(imdbID) {
+    debugger;
+    const imdb=imdbID
+    return axios.get(Movies_CRUD_URL+"/MovieDetail/",{ params: { imdb }});
+}
